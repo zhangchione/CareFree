cd9730b1328160376001f6c06b4d4339c0e69c7b
zhangchione <1525163730@qq.com>
2019-07-14 20:49:09 +0800

    feat:b = 1

--- CareFree/AppDelegate.swift
+++ CareFree/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     let a = 1
+    let b = 1
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let realm = try! Realm()
         print(realm.configuration.fileURL!)
